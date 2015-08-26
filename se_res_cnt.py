6653ecb74eed0083ca96b811e15df4ee10f48238
keyanwang <keyan.wang@anytum.com>
2015-08-26 18:44:11 +0800

    'update'

--- se_res_cnt.py
+++ se_res_cnt.py
@@ -1,2 +1,15 @@
-#coding=utf-8
-print 'ÄãºÃ'
+# -*- coding: gbk -*-
+import urllib2
+import re
+
+
+def search(keyword):
+    url = 'http://www.baidu.com/s?wd=' + keyword
+    html = urllib2.urlopen(url).read()
+    f = open('nihao.html','wb')
+    f.write(html)
+    f.close()
+
+if __name__ == '__main__':
+    keyword = raw_input('enter key word:')
+    search(keyword)
